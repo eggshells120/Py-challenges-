47a8e428f88f319b0b80e72ae003269175d8fe69
eggshells120 <134540918+eggshells120@users.noreply.github.com>
2023-11-08 18:03:48 -0800

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,42 +1,62 @@
-import pandas as pd
+import csv
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv("election_data.csv")
+# Define the input file and output file names
+input_file = "election_data.csv"
+output_file = "election_results.txt"
 
-# Calculate the total number of votes cast
-total_votes = len(df)
+# Initialize variables to store the election data
+total_votes = 0
+candidates = {}
+winner = ""
+winner_votes = 0
 
-# Create a DataFrame with candidate names and their respective vote counts
-candidate_counts = df["Candidate"].value_counts().reset_index()
-candidate_counts.columns = ["Candidate", "Vote Count"]
+# Read the CSV file and calculate the election values
+with open(input_file, "r") as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader)  # Skip the header row
 
-# Calculate the percentage of votes each candidate won
-candidate_counts["Percentage"] = (candidate_counts["Vote Count"] / total_votes) * 100
+    for row in csvreader:
+        total_votes += 1
+        candidate_name = row[2]
 
-# Find the winner based on the popular vote
-winner = candidate_counts.iloc[candidate_counts["Vote Count"].idxmax()]
+        if candidate_name not in candidates:
+            candidates[candidate_name] = 0
+        candidates[candidate_name] += 1
 
-# Print the analysis to the terminal
+# Calculate the election results
+results = []
+for candidate, votes in candidates.items():
+    percentage = (votes / total_votes) * 100
+    results.append((candidate, percentage, votes))
+
+# Find the winner
+for candidate, votes in candidates.items():
+    if votes > winner_votes:
+        winner = candidate
+        winner_votes = votes
+
+# Print the election results to the terminal
 print("Election Results")
-print("-------------------------")
+print("----------------------------")
 print(f"Total Votes: {total_votes}")
-print("-------------------------")
-for index, row in candidate_counts.iterrows():
-    print(f"{row['Candidate']}: {row['Percentage']:.3f}% ({row['Vote Count']})")
-print("-------------------------")
-print(f"Winner: {winner['Candidate']}")
-print("-------------------------")
+print("----------------------------")
+for candidate, percentage, votes in results:
+    print(f"{candidate}: {percentage:.3f}% ({votes})")
+print("----------------------------")
+print(f"Winner: {winner}")
+print("----------------------------")
 
-# Export the results to a text file
-with open("election_results.txt", "w") as textfile:
-    textfile.write("Election Results\n")
-    textfile.write("-------------------------\n")
-    textfile.write(f"Total Votes: {total_votes}\n")
-    textfile.write("-------------------------\n")
-    for index, row in candidate_counts.iterrows():
-        textfile.write(f"{row['Candidate']}: {row['Percentage']:.3f}% ({row['Vote Count']})\n")
-    textfile.write("-------------------------\n")
-    textfile.write(f"Winner: {winner['Candidate']}\n")
-    textfile.write("-------------------------")
+# Export the election results to a text file
+with open(output_file, "w") as txtfile:
+    txtfile.write("Election Results\n")
+    txtfile.write("----------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write("----------------------------\n")
+    for candidate, percentage, votes in results:
+        txtfile.write(f"{candidate}: {percentage:.3f}% ({votes})\n")
+    txtfile.write("----------------------------\n")
+    txtfile.write(f"Winner: {winner}\n")
+    txtfile.write("----------------------------\n")
 
-
+# Print a confirmation message
+print(f"Results exported to {output_file}")
